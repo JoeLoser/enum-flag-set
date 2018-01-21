222c3f3b2948f309a8a12672b0285ff68d58ee2f
Joe Loser <joeloser93@gmail.com>
2018-01-21 17:22:02 -0500

    [Refactor] Remove unused typedefs
    When starting the flag_set, I was thinking of it making it a true
    container and started to define some sensible typedefs, but did not
    continue with defining the iterator typedefs needed. So, for now,
    they are just noise and in the way. So, we remove them.
    
    At a later point, we may define the full typedefs needed to be
    a container.

--- include/flag_set.hpp
+++ include/flag_set.hpp
@@ -22,10 +22,6 @@ namespace flag_set
             using underlying_type = typename std::underlying_type_t<enum_type>;
             using rep_type = typename std::make_unsigned_t<underlying_type>;
 
-            using pointer = enum_type*;
-            using const_pointer = const enum_type*;
-            using size_type = std::size_t;
-
             // Default main 5
             flag_set() noexcept = default;
             flag_set(const flag_set&) noexcept = default;
